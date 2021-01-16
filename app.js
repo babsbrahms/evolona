2300cea0a5bc397c56969da9f90303d865eacb5c
olayinka <babsvanbrahms@gmail.com>
2021-01-16 10:39:27 +0100

    Next account

--- app.js
+++ app.js
@@ -75,13 +75,29 @@ app.use('/cms', cmsRouter);
 /* GET home page. */
 app.get('/', function(req, res, next) {
   
-  Product.find({}).sort({ _id: -1}).exec(function(err, clothings) {
-    if (err) {
-       throw err
-    } else{
-      
-      res.render('index', { page: '', clothings });
-    }
+  const {nav} = req.query;
+  let current = req.session.blog_number || 0;
+  if (nav === 'previous' && (current !== 0) ) {
+    current = current - 1;
+  } else if (nav === 'next') {
+    current = current + 1;
+  } 
+
+  req.session.blog_number = current;
+  let limit = 10;
+  let skip = limit*current;
+  Product.find({}).sort({ _id: -1 }).skip(skip).limit(limit).exec(function (err, clothings) {
+      if (err) {
+        res.locals.error = req.app.get('env') === 'development' ? err||errs : {};
+
+        res.render('/', { page: '', message: err.message});
+      } else if ((clothings.length === 0) && (current !== 0)) {
+        let count = (current > 0)? current - 1 : 0
+        req.session.blog_number = count;
+        res.redirect('/')
+      } else {
+        res.render('index', { page: '', clothings, current_number: current, limit });
+      }
   })
 });
 
